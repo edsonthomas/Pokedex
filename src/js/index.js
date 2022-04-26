f3cdbb2b47764aa5d4a82e4f7aca3d98e8c66e4c
Edson Thomas <edsonthomas.dev@gmail.com>
2022-04-25 22:07:32 -0300

    primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/*
+quando clicar no pokémon da listagem temos que esconder o cartão do pokémon aberto e mostrar o cartão correspondente ao que foi selecionado na listagem
+
+pra isso vamos precisar trabalhar com dois elementos
+1- listagem
+2- cartão pokémon
+
+
+ */
+
+const nome = 'Edson'
+console.log(nome)
